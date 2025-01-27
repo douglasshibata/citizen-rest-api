e402206264495968880f2f0caab7c119f098fec7
Douglas Shibata <douglas.shibata11@gmail.com>
2025-01-27 19:49:20 -0300

    feat: init project

---
+++ src/main/java/br/edu/unyleya/aluno/citizen_rest_api/CitizenRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unyleya.aluno.citizen_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenRestApiApplication.class, args);
+	}
+
+}
